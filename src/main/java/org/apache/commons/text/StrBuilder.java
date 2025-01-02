6f23a9dbddda4ce5f771cfceeede3aaa1693f2a6
Carmine Calabrese <carmine.calabrese99@gmail.com>
2025-01-02 16:46:10 +0100

    Fixing Quality Gate fail (#5)

--- src/main/java/org/apache/commons/text/StrBuilder.java
+++ src/main/java/org/apache/commons/text/StrBuilder.java
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-//sonar.issue.ignore.allfile
 package org.apache.commons.text;
 
 import java.io.IOException;
